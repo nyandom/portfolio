80789600b36fd0784bbf038f81289ef09de58251
pupxrl <nyan@incel.email>
2025-01-04 01:29:19 +0000

    Styles

--- scripts/App.js
+++ scripts/App.js
@@ -23,32 +23,37 @@ for (let i = 0; i < gridItems.length; i++) {
     const header = document.createElement("h2");
     popupContainer.appendChild(header);
 
-    if (itemNumber === "1") {
+    if (itemNumber === "Education") {
       header.innerText = "Education";
+      header.style.paddingLeft = "5px";
       const content = document.createElement("div");
-      content.innerText = "Imperial College London Web Development";
+      content.innerText =
+        "Certificate in Full Stack Web Development from Imperial College London.";
       content.style.backgroundColor = "rgba(25, 25, 25, 0.6)";
       content.style.padding = "20px";
       content.style.borderRadius = "10px";
       content.style.width = "320px";
-      content.style.height = "20px";
+      content.style.height = "40px";
       content.style.fontFamily = "Monsterrat, sans-serif";
       popupContainer.appendChild(content);
-    } else if (itemNumber === "2") {
+    } else if (itemNumber === "About Me") {
       header.innerText = "About me";
+      header.style.paddingLeft = "19px";
       const content = document.createElement("div");
-      content.innerText =
-        "Hi, I'm Nyan! A passionate web developer with over 4 years of experience, I specialize in creating dynamic, user-friendly applications. Web development isn’t just my career, it's my life! I’m constantly improving my skills. My expertise lies in the MERN stack (MongoDB, Express, React, Node.js), and I love using these tools to build stylish and efficient websites.\nOutside of coding, I enjoy listening to music and hanging out with friends, but my mind is always focused on finding new ways to be creative when building websites.";
+      content.innerHTML = `<p>Hi, I'm Nyan! A passionate web developer with over <strong>4 years of experience</strong>, I specialize in creating <strong>efficient, user-friendly applications</strong>.</p>
+    <p>Web development isn’t just my career, <strong>it's my life!</strong> I’m constantly improving my skills. My expertise lies in the <strong>MERN stack</strong> (MongoDB, Express, React, Node.js), and I love using these tools to build stylish and professional websites.</p>
+    <p>Outside of coding, I enjoy listening to music and hanging out with friends, but my mind is always focused on finding new ways to be creative when building websites.</p>`;
       content.style.backgroundColor = "rgba(25, 25, 25, 0.6)";
       content.style.padding = "20px";
       content.style.borderRadius = "10px";
       content.style.width = "500px";
-      content.style.height = "195px";
+      content.style.height = "280px";
       content.style.fontFamily = "Monsterrat, sans-serif";
       content.style.lineHeight = "23px";
       popupContainer.appendChild(content);
-    } else if (itemNumber === "3") {
+    } else if (itemNumber === "Projects") {
       header.innerText = "Projects";
+      header.style.paddingLeft = "5px";
       const content = document.createElement("div");
       content.innerHTML =
         '<a href="https://discord.com/application-directory/1136001498728386610">Check out Cash Discord Bot ↗</a><br>More projects being uploaded soon..';
@@ -60,16 +65,18 @@ for (let i = 0; i < gridItems.length; i++) {
       content.style.fontFamily = "Monsterrat, sans-serif";
       content.style.lineHeight = "23px";
       popupContainer.appendChild(content);
-    } else if (itemNumber === "4") {
+    } else if (itemNumber === "Technical Skills") {
       header.innerText = "Technical Skills";
+      header.style.paddingLeft = "10px";
       const content = document.createElement("div");
-      content.innerHTML =
-        "I am proficient in HTML, CSS, and JavaScript, with solid experience in front-end development. I am skilled in using modern JavaScript frameworks and libraries, including React and Vue.js, and I have hands-on experience with the MERN stack (MongoDB, Express, React, Node.js) for full-stack development. I am experienced in version control with Git, enabling efficient collaboration and code management.";
+      content.innerHTML = `<p>I am proficient in <strong>HTML, CSS, and JavaScript</strong>, with solid experience in front-end development.</p>
+    <p>I am skilled in using modern JavaScript frameworks and libraries, including <strong>React</strong> and <strong>Vue.js</strong>, and I have hands-on experience with the <strong>MERN stack</strong> (MongoDB, Express, React, Node.js) for full-stack development.</p>
+    <p>I am experienced in version control with <strong>Git</strong>, enabling efficient collaboration and code management.</p>`;
       content.style.backgroundColor = "rgba(25, 25, 25, 0.6)";
       content.style.padding = "20px";
       content.style.borderRadius = "10px";
       content.style.width = "450px";
-      content.style.height = "150px";
+      content.style.height = "240px";
       content.style.fontFamily = "Monsterrat, sans-serif";
       content.style.lineHeight = "23px";
       popupContainer.appendChild(content);
